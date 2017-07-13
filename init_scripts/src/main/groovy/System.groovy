1d2567a1664335feb73551dc0bfcec08fbea18a2
Oleg Nenashev <o.v.nenashev@gmail.com>
2017-07-13 09:06:48 +0200

    Disable Remoting CLI, JNLP1/2, enable S2M protection

--- init_scripts/src/main/groovy/System.groovy
+++ init_scripts/src/main/groovy/System.groovy
@@ -1,8 +1,10 @@
-import jenkins.model.Jenkins;
+import jenkins.model.Jenkins
+import jenkins.CLI
+import org.kohsuke.stapler.StaplerProxy
 
 boolean allowRunsOnMaster = Boolean.getBoolean("io.jenkins.dev.security.allowRunsOnMaster");;
 
-println("--- System configuration")
+println("-- System configuration")
 
 // TODO: Job Restrictions
 if (allowRunsOnMaster) {
@@ -11,3 +13,9 @@ if (allowRunsOnMaster) {
 }
 
 // TODO: Configure Job Restrictions, Script Security, Authorize Project, etc., etc.
+println("--- Configuring Remoting (JNLP4, no Remoting CLI)")
+CLI.get().setEnabled(false);
+Jenkins.instance.setAgentProtocols(new HashSet<String>(Arrays.asList("JNLP4-connect")));
+Jenkins.instance.getExtensionList(StaplerProxy.class)
+        .get(jenkins.security.s2m.AdminWhitelistRule.class)
+        .setMasterKillSwitch(false)
