d1972c160d66385e0aac56cdcb9ad80e388f88aa
Oleg Nenashev <o.v.nenashev@gmail.com>
2017-07-21 18:06:24 +0300

    Docker: refactor the configuration to the With syntax

--- init_scripts/src/main/groovy/Docker.groovy
+++ init_scripts/src/main/groovy/Docker.groovy
@@ -13,60 +13,71 @@ import jenkins.model.Jenkins
 import jenkins.model.JenkinsLocationConfiguration
 
 println("=== Setting Jenkins URL")
-String host = java.lang.System.getProperty("io.jenkins.dev.host");
+String host = java.lang.System.getProperty("io.jenkins.dev.host")
 if (host == null) {
     host = "localhost"
 } else {
-    JenkinsLocationConfiguration.get().setUrl("http://${host}:8080");
+    JenkinsLocationConfiguration.get().setUrl("http://${host}:8080")
 }
 
 println("=== Installing Docker Cloud for Linux nodes")
 
 //TODO: YAD Plugin does not work well with this image and Unix sockets. Would be useful to migrate
 
-final DockerConnector connector = new DockerConnector("tcp://${host}:2376");
+final DockerConnector connector = new DockerConnector("tcp://${host}:2376")
 
-final DockerPullImage pullImage = new DockerPullImage();
-pullImage.setPullStrategy(DockerImagePullStrategy.PULL_ONCE);
+final DockerPullImage pullImageOpt = new DockerPullImage()
+pullImageOpt.pullStrategy = DockerImagePullStrategy.PULL_ONCE
 
-final DockerComputerJNLPLauncher launcher = new DockerComputerJNLPLauncher();
-launcher.setLaunchTimeout(100);
-launcher.setUser("jenkins");
-launcher.setJenkinsUrl(JenkinsLocationConfiguration.get().getUrl());
-launcher.setNoCertificateCheck(true);
+final DockerComputerJNLPLauncher jnlpLauncher = new DockerComputerJNLPLauncher()
+jnlpLauncher.with {
+    launchTimeout = 100
+    user = "jenkins"
+    jenkinsUrl = JenkinsLocationConfiguration.get().getUrl()
+    noCertificateCheck = true
+}
 
-final DockerCreateContainer createContainer = new DockerCreateContainer();
-createContainer.setPrivileged(false);
-createContainer.setTty(false);
-//TODO: Make shared local Maven repo configurable
-createContainer.setVolumes(Collections.singletonList("maven-repo:/root/.m2"));
-//createContainer.setVolumesFrom(Collections.singletonList("maven-repo:/root/.m2"));
+final DockerCreateContainer createContainerOpt = new DockerCreateContainer()
+createContainerOpt.with {
+    privileged = false
+    tty = false
+    //TODO: Make shared local Maven repo configurable
+    volumes = Arrays.asList("maven-repo:/root/.m2", "jar-cache:/root/.jenkins")
+}
 
-final DockerStopContainer stopContainer = new DockerStopContainer();
-stopContainer.setTimeout(100);
+final DockerStopContainer stopContainerOpt = new DockerStopContainer()
+stopContainerOpt.with {
+    timeout = 100
+}
 
-final DockerRemoveContainer removeContainer = new DockerRemoveContainer();
-removeContainer.setForce(true);
-removeContainer.setRemoveVolumes(true);
+final DockerRemoveContainer removeContainerOpt = new DockerRemoveContainer()
+removeContainerOpt.with {
+    force = true
+    removeVolumes = true
+}
 
-final DockerContainerLifecycle containerLifecycle = new DockerContainerLifecycle();
-containerLifecycle.setImage("jenkinsci/jnlp-slave");
-containerLifecycle.setPullImage(pullImage);
-containerLifecycle.setCreateContainer(createContainer);
-containerLifecycle.setStopContainer(stopContainer);
-containerLifecycle.setRemoveContainer(removeContainer);
+final DockerContainerLifecycle containerLifecycle = new DockerContainerLifecycle()
+containerLifecycle.with {
+    image = "jenkinsci/jnlp-slave"
+    pullImage = pullImageOpt
+    createContainer = createContainerOpt
+    stopContainer = stopContainerOpt
+    removeContainer = removeContainerOpt
+}
 
-final DockerSlaveTemplate dockerSlaveTemplate = new DockerSlaveTemplate();
-dockerSlaveTemplate.setDockerContainerLifecycle(containerLifecycle);
-dockerSlaveTemplate.setLabelString("linux");
-dockerSlaveTemplate.setLauncher(launcher);
-dockerSlaveTemplate.setMaxCapacity(10);
-dockerSlaveTemplate.setMode(hudson.model.Node.Mode.EXCLUSIVE)
-dockerSlaveTemplate.setNumExecutors(1);
-dockerSlaveTemplate.setRetentionStrategy(new DockerOnceRetentionStrategy(30));
+final DockerSlaveTemplate dockerSlaveTemplate = new DockerSlaveTemplate()
+dockerSlaveTemplate.with {
+    dockerContainerLifecycle = containerLifecycle
+    labelString = "linux"
+    launcher = jnlpLauncher
+    maxCapacity = 10
+    mode = hudson.model.Node.Mode.EXCLUSIVE
+    numExecutors = 1
+    retentionStrategy = new DockerOnceRetentionStrategy(30)
+}
 
-final ArrayList<DockerSlaveTemplate> dockerSlaveTemplates = new ArrayList<>();
-dockerSlaveTemplates.add(dockerSlaveTemplate);
+final ArrayList<DockerSlaveTemplate> dockerSlaveTemplates = new ArrayList<>()
+dockerSlaveTemplates.add(dockerSlaveTemplate)
 
-def yadCloud = new DockerCloud("docker-cloud", dockerSlaveTemplates, 10, connector);
-Jenkins.instance.clouds.add(yadCloud);
+def yadCloud = new DockerCloud("docker-cloud", dockerSlaveTemplates, 10, connector)
+Jenkins.instance.clouds.add(yadCloud)
