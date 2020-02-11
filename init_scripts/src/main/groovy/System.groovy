279dadaefa4ae7550061c145c8d3c864664d99f6
WolverMinion <mikeschmid71@googlemail.com>
2020-02-11 20:01:15 +0100

    empty hooks

--- init_scripts/src/main/groovy/System.groovy
+++ init_scripts/src/main/groovy/System.groovy
@@ -1,11 +1,12 @@
 import hudson.security.csrf.DefaultCrumbIssuer
 import jenkins.model.Jenkins
 import jenkins.model.JenkinsLocationConfiguration
-//import jenkins.CLI //class is deleted
 import jenkins.security.s2m.AdminWhitelistRule
 import org.kohsuke.stapler.StaplerProxy
+/*
 import hudson.tasks.Mailer
 import hudson.plugins.locale.PluginImpl
+*/
 
 println """
 ###############################
@@ -17,7 +18,6 @@ println("-- System configuration")
 
 // TODO: Configure Job Restrictions, Script Security, Authorize Project, etc., etc.
 println("--- Configuring Remoting (JNLP4 only, no Remoting CLI)")
-//CLI.get().enabled = false //class is deleted
 Jenkins.getInstanceOrNull().getExtensionList(StaplerProxy.class)
     .get(AdminWhitelistRule.class)
     .masterKillSwitch = false
@@ -27,14 +27,18 @@ println("--- Configuring Quiet Period")
 Jenkins.getInstanceOrNull().quietPeriod = 0
 
 println("--- Configuring Email global settings")
+/*
 JenkinsLocationConfiguration.get().adminAddress = "admin@non.existent.email"
 Mailer.descriptor().defaultSuffix = "@non.existent.email"
+*/
 
 println("--- Configuring Locale")
 //TODO: Create ticket to get better API
+/*
 PluginImpl localePlugin = (PluginImpl)Jenkins.getInstanceOrNull().getPlugin("locale")
 localePlugin.systemLocale = "de_DE"
 localePlugin.@ignoreAcceptLanguage=true
+*/
 
 println """
 ###############################
