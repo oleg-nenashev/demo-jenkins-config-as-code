18613b4957dfecf544e927b3ac3b152d3eecb502
WolverMinion <mikeschmid71@googlemail.com>
2020-02-15 19:21:49 +0100

    refactoring

--- build/maven/mvn-deploy.sh
+++ build/maven/mvn-deploy.sh
@@ -3,6 +3,4 @@ BASEDIR=$(dirname "$0")
 
 . ${BASEDIR}/mvn-init.sh "$@"
 
-#  -Dgithub.username=${GITHUB_USER} -Dgithub.password=${GITHUB_PWD}"
-
 runMvnw '-Dmaven.wagon.http.pool=false clean deploy'
